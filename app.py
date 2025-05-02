c763c9e7dbbc11944571d60b38124c8bb0bc1341
BobbyYan-Burnside <21589@burnside.school.nz>
2025-05-02 20:05:31 +1200

    Create a function to automatically connect and query

--- app.py
+++ app.py
@@ -6,23 +6,34 @@ app = Flask(__name__)
 #path and filename for the database
 DATABASE = "musicsheethub.db"
 
+# function to automatically connect and query
+def query_db(sql, args=(), one=False):
+    '''connect and query-will return one item if one=true and can accept arguments as tuple'''
+    # Connect to the database file
+    db = sqlite3.connect(DATABASE)
+    # create the cursor
+    cursor = db.cursor()
+    # excute the query
+    cursor.execute(sql, args)
+    # fetch all the results of the query
+    results = cursor.fetchall()
+    # save the data
+    db.commit()
+    # close the database
+    db.close()
+    # return None if there is no result from the query
+    # return the first item only if one=True
+    # return the list of tuples if one=False
+    return(results[0] if results else None) if one else results
+
+
 #route go here
 @app.route("/")
 def index():
-    # Connect to the database file
-    db = sqlite3.connect(DATABASE)
-    # Create the cursor
-    cursor = db.cursor()
-    # Store a query as a string variable
-    sql = "SELECT * FROM Users"
-    # Excute the query
-    cursor.execute(sql)
-    # Fetch all the results of the query
-    results = cursor.fetchall()
-    # Close the database
-    db.close()
-    # Send the data to the template
+    sql = "SELECT * FROM Sheets"
+    results = query_db(sql)
     return render_template('index.html', results=results)
+
 
 # this is the app with debug on
 if __name__ == "__main__":
